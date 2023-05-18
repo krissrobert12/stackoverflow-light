e1aa13facd1722b873927dd73512980403368e38
Kriss Gardner <krissrobert12@gmail.com>
2023-05-18 18:55:24 +0300

    feat: create question routing

---
+++ src/routes/v1/question.route.ts
@@ -0,0 +1,28 @@
+import express, { Router } from 'express';
+import { validate } from '../../modules/validate';
+import { auth } from '../../modules/auth';
+import { questionController, questionValidation } from '../../modules/question';
+
+const router: Router = express.Router();
+
+router
+  .route('/')
+  .post(auth('generic'), validate(questionValidation.createQuestion), questionController.createQuestion)
+  .get(auth('generic'), validate(questionValidation.getQuestions), questionController.getQuestions);
+
+router
+  .route('/:questionId')
+  .get(auth('generic'), validate(questionValidation.getQuestion), questionController.getQuestion)
+  .patch(auth('generic'), validate(questionValidation.updateQuestion), questionController.updateQuestion)
+  .delete(auth('generic'), validate(questionValidation.deleteQuestion), questionController.deleteQuestion);
+
+export default router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Questions
+ *   description: Question management and retrieval
+ */
+
+// TODO: Write swagger for this
